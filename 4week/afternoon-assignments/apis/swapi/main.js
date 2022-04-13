beb2eaebd76d121f66f265fadcc82ed435acc9a0
John Booth <jhunter.booth@gmail.com>
2022-04-13 16:55:33 -0600

    finished project

--- 4week/afternoon-assignments/apis/swapi/main.js
+++ 4week/afternoon-assignments/apis/swapi/main.js
@@ -1,11 +1,16 @@
-
-
 const button = document.querySelector("button")
 
 const buttonClicked = () =>{
     axios.get("https://swapi.dev/api/planets/2/").then(res=>{
-        let resident = res.data.results[0].residents
-        console.log(resident)
+        let resident = res.data.residents
+        // console.log(resident)
+        for (let i = 0; i < resident.length; i++){
+            axios.get(`${resident[i]}`).then(res=>{
+                let residentName = document.createElement("h2")
+                residentName.innerText = res.data.name
+                document.body.appendChild(residentName)
+            })
+        }
     })
 }
 
