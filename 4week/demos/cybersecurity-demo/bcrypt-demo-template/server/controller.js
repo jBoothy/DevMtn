4a060d19b7aa4ba021f9cd22b663d83bfa3a32f3
John Booth <jhunter.booth@gmail.com>
2022-04-14 12:51:30 -0600

    commit

--- 4week/demos/cybersecurity-demo/bcrypt-demo-template/server/controller.js
+++ 4week/demos/cybersecurity-demo/bcrypt-demo-template/server/controller.js
@@ -1,9 +1,10 @@
-
+const bcrypt = require("bcryptjs")
+const chats = []
 
 module.exports = {
     createMessage: (req, res) =>{
         console.log(req.body)
         const {pin, message} = req.body
-        
+        const salt = bcrypt.getSaltSync()
     }
 }
