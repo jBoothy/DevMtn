68dfdae55eaa8d67c2f21618352fd4379f2c32c5
John Booth <jhunter.booth@gmail.com>
2022-04-14 12:44:10 -0600

    commit

--- 4week/demos/cybersecurity-demo/bcrypt-demo-template/server/controller.js
+++ 4week/demos/cybersecurity-demo/bcrypt-demo-template/server/controller.js
@@ -1,9 +1,7 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-app.use(express.json())
-app.use(cors())
 
 
-
-app.listen(4004, ()=>console.log("Server running on port 4004"))
+module.exports = {
+    createMessage: (req, res) =>{
+        console.log(req.body)
+    }
+}
