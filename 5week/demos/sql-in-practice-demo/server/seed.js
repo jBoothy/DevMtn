2ab85fed76100c8c0eb760d8bb4666cdab1b58d2
John Booth <jhunter.booth@gmail.com>
2022-04-20 13:01:02 -0600

    finished demo

--- 5week/demos/sql-in-practice-demo/server/seed.js
+++ 5week/demos/sql-in-practice-demo/server/seed.js
@@ -1,3 +1,17 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
+
+const {CONNECTION_STRING} = process.env
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
 module.exports = {
     seed: (req, res) => {
         sequelize.query(`
