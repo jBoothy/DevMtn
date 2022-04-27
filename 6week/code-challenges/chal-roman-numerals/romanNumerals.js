9db1addf52c3f79980070e81ae091367638b1343
John Booth <jhunter.booth@gmail.com>
2022-04-27 10:02:56 -0600

    finished challenge

--- 6week/code-challenges/chal-roman-numerals/romanNumerals.js
+++ 6week/code-challenges/chal-roman-numerals/romanNumerals.js
@@ -51,7 +51,29 @@ toRoman(99)
 */
 
 const toRoman=(input)=>{
+ let roman = {
+     M: 1000,
+     CM: 900,
+     D: 500,
+     CD: 400,
+     C: 100,
+     XC: 90,
+     L: 50,
+     XL: 40,
+     X: 10,
+     IX: 9,
+     V: 5,
+     IV: 4,
+     I: 1
+ };
+ let solution = '';
 
+ for(let i of Object.keys(roman)){
+     let q = Math.floor(input/roman[i])
+     input -= q * roman[i]
+     solution += i.repeat(q)
+ }
+ return solution;
 }
 
 console.log(toRoman(5))
